305c5706437e325d27517e101acb543f384ce227
docfr10 <docfr10@yandex.ru>
2024-12-02 15:08:47 +0300

    Add intents

--- app/src/main/java/com/example/lesson_sharedpreferences/MainActivity.kt
+++ app/src/main/java/com/example/lesson_sharedpreferences/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.lesson_sharedpreferences
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.example.lesson_sharedpreferences.databinding.ActivityMainBinding
@@ -16,6 +18,8 @@ class MainActivity : AppCompatActivity() {
             if (result.resultCode == RESULT_OK) {
                 // Обновление UI при получении ответа от SettingsActivity
                 updateUI()
+                // Обновление картинки после выбора
+                mainBinding.imageView.setImageURI(result.data?.data)
             }
         }
 
@@ -32,12 +36,44 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, SettingsActivity::class.java)
             startForResult.launch(intent) // Запуск активности с результатом
         }
+
+        // Переход на веб-сайт
+        mainBinding.button.setOnClickListener {
+            val webIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com"))
+            startActivity(webIntent)
+        }
+
+        // Отправка сообщения через другие приложения
+        mainBinding.button2.setOnClickListener {
+            val intentMessage = Intent(Intent.ACTION_SEND).apply {
+                type = "text/*"
+                putExtra(
+                    Intent.EXTRA_TEXT,
+                    getSharedPreferences(
+                        "user_preferences",
+                        Context.MODE_PRIVATE
+                    ).getString("username", "Guest")
+                )
+            }
+            // Выбор приложения, через котрое отправим сообщение
+            startActivity(Intent.createChooser(intentMessage, "Отправить через:"))
+        }
+
+        // Выбор изображения из галереи
+        mainBinding.button3.setOnClickListener {
+            val intentImage =
+                Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            startForResult.launch(intentImage)
+        }
     }
 
     // Функция для обновления UI
     private fun updateUI() {
+        // Создание хранилища SharedPreferences с названием user_preferences и закрытым уровнем доступа
         val sharedPreferences = getSharedPreferences("user_preferences", Context.MODE_PRIVATE)
+        // Обращение к элементу хранилища username
         val username = sharedPreferences.getString("username", "Guest")
+        // Обращение к элементу хранилища dark_mode
         val isDarkMode = sharedPreferences.getBoolean("dark_mode", false)
 
         mainBinding.textViewUsername.text = "Username: $username"
